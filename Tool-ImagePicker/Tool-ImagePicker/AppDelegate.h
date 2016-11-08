0c05a315db5d004dbf758479e323cc1e34e05cee
杨常勇 <chang-yong.yang@utsoft.cn>
2016-11-08 14:26:34 +0800

    修改文件路径

---
+++ Tool-ImagePicker/Tool-ImagePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tool-ImagePicker
+//
+//  Created by yons on 16/11/8.
+//  Copyright © 2016年 yons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tool-ImagePicker/Tool-ImagePicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tool-ImagePicker
+//
+//  Created by yons on 16/11/8.
+//  Copyright © 2016年 yons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
